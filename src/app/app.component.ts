5730e4ae9d26b9fbbeaf95fad3889857db141e44
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-03-07 15:28:01 +0300

    Toastr Service Implemention

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ETicaretClient';
+  constructor(private toastrService:CustomToastrService){
+
+    toastrService.message("Merhaba","Burak",{
+      messageType:ToastrMessageType.Error,
+      position:ToastrPosition.TopCenter
+    })
+
+    toastrService.message("Merhaba","Mithat",{
+      messageType:ToastrMessageType.Info,
+      position:ToastrPosition.TopCenter
+    })
+
+    toastrService.message("Merhaba","Mehmetcan",{
+      messageType:ToastrMessageType.Success,
+      position:ToastrPosition.BottomLeft
+    })
+
+    toastrService.message("Merhaba","Samet",{
+      messageType:ToastrMessageType.Info,
+      position:ToastrPosition.BottomCenter
+    })
+  
+
+  }
 }
 
 
